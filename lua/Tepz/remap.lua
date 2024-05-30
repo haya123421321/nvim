6de2bafa202d05ab3ee2406585a73354422972e5
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-05-30 03:41:07 +0200

    Add files via upload

---
+++ lua/Tepz/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>x', ':!%:p<CR>')
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
