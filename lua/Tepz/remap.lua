43f117c16dbde4bbd0149c63b8ef4cfa95ad2064
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-05-31 04:27:38 +0200

    Add files via upload

---
+++ lua/Tepz/remap.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>x', ':!%:p<CR>')
+vim.keymap.set("v", "<S-Tab>", "<gv")
+
+vim.keymap.set("i", "<M-BS>", "<C-o>:normal! bdiw<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("v", "<C-S-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("v", "<C-S-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-S-Up>", "<Esc>:m .-2<CR>==gi", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-S-Down>", "<Esc>:m .+1<CR>==gi", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-Up>", ":m .-2<CR>==", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-Down>", ":m .+1<CR>==", { noremap = true, silent = true })
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
