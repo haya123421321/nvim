b1c12f815053c285c1f2b3b522009342065b6067
haya123421321 <90647360+haya123421321@users.noreply.github.com>
2024-06-05 14:04:11 +0200

    Update remap.lua

--- lua/Tepz/remap.lua
+++ lua/Tepz/remap.lua
@@ -5,7 +5,7 @@ vim.keymap.set("n", "<leader>q", vim.cmd.Ex)
 vim.keymap.set('n', '<leader>x', ':!%:p<CR>')
 vim.keymap.set("v", "<S-Tab>", "<gv")
 
-vim.keymap.set("i", "<M-BS>", "<C-o>:normal! bdiw<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<M-BS>", "<C-o>:normal! db<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "<C-S-Down>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<C-S-Up>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
